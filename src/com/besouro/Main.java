d04e14e9b1ebb937f591e8cc80535d19672566fc
Luiza Munk <luiza.munk@gmail.com>
2017-01-22 15:59:42 -0200

    Add dog class

--- src/com/besouro/Main.java
+++ src/com/besouro/Main.java
@@ -1,8 +1,16 @@
 package com.besouro;
 
+import com.besouro.model.Dog;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        Dog doggo = new Dog(5,"doggo","black");
+        Dog brisa = new Dog(2,"brisa","brown");
+        doggo.printDog();
+        brisa.printDog();
+
+
     }
 }
